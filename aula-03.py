302a1f76b343183ded2ae4eab51373c178ae6fa1
Julia <jrhuliafy@gmail.com>
2025-03-10 19:54:18 -0300

    :pencil: exercicio maioridade

---
+++ aula-03.py
@@ -0,0 +1,16 @@
+# Desafio
+# Criar um programa que para verificar a maioridade
+
+# Passo 1: Criar uma variável
+# Passo 2: Criar uma estrutura de condição para verificar a idade
+# Passo 3: Imprimir o resultado adequadamente de acordo com a idade da pessoa
+
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade")
+
+elif idade >=12:
+    print("Você é adolescente")
+else:
+    print("Você é criança")
